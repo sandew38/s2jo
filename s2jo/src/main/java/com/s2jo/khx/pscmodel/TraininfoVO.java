f821f57cd0ca1c082e4dce53a2eea0231e7ad1ec
user1 <user1@user1-PC>
2017-06-12 17:08:29 +0900

    20170612--박수창
    170
    업데이트내용 : model단 패키지 생성함

---
+++ s2jo/src/main/java/com/s2jo/khx/pscmodel/TraininfoVO.java
@@ -0,0 +1,50 @@
+package com.s2jo.khx.pscmodel;
+
+public class TraininfoVO {
+	
+	private String traintype;			// ê¸°ì°¨ì¢…ë¥˜ (1: ktx, 2: 3: )
+	private String trainno;				// ?—´ì°¨ë²ˆ?˜¸
+	private String perminuterate;		// ë¶„ë‹¹?š´?„ 
+	
+	RuninfoVO RuninfoVO;
+	
+	public TraininfoVO()
+	{	// ê¸°ë³¸ ?ƒ?„±?
+		
+	}
+	
+	public TraininfoVO(String traintype, String trainno, String perminuterate) {
+		super();
+		this.traintype = traintype;
+		this.trainno = trainno;
+		this.perminuterate = perminuterate;
+	}
+
+	public String getTraintype() {
+		return traintype;
+	}
+
+	public void setTraintype(String traintype) {
+		this.traintype = traintype;
+	}
+
+	public String getTrainno() {
+		return trainno;
+	}
+
+	public void setTrainno(String trainno) {
+		this.trainno = trainno;
+	}
+
+	public String getPerminuterate() {
+		return perminuterate;
+	}
+
+	public void setPerminuterate(String perminuterate) {
+		this.perminuterate = perminuterate;
+	}
+	
+	
+	
+	
+}
